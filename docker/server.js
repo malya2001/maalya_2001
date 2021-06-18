c8b8c8aa3f3bddd51bcecb5e0384e727d9e66121
Dumitrash <saratinean1.9@gmail.com>
2021-06-19 02:24:53 +0300

    first commit

---
+++ docker/server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8080;
+const Host = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+var os = require("os");
+  res.send(Hello world ' + os.hostname() + '\n');
+ 
+ });
+ 
+ app.listen(PORT, HOST);
+ console.log('Running on http://${HOST}:${PORT}');
